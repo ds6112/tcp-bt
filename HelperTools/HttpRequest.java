738990e3bff63a9cb27fed9c0f3008371d816422
Dan <danielsu6401@gmail.com>
2014-09-19 15:54:52 -0400

    added helper

---
+++ HelperTools/HttpRequest.java
@@ -0,0 +1,50 @@
+package HelperTools;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import javax.net.ssl.HttpsURLConnection;
+import java.net.URL;
+ 
+
+ 
+public class HttpRequest {
+ 
+	
+ 
+	// HTTP GET request
+	public static void sendGet() throws Exception {
+ 		String USER_AGENT = "Mozilla/5.0";
+		String url = "http://www.google.com/search?q=mkyong";
+ 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		// optional default is GET
+		con.setRequestMethod("GET");
+ 
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+ 
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());
+ 
+	}
+ 
+
+ 
+}
---
+++ RUBTClient.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import GivenTools.*;
+import HelperTools.*;
+//import HelperTools.*;
+
+public class RUBTClient {
+
+    public static void main(String[] args) {
+        
+        //Check if the arguments are valid
+        if ( args.length < 2 || args.length > 2){
+            System.out.println("Usage: RUBTClient torrent_file file_save_name\n");
+            return;
+        }
+        else {
+            
+            // Check if first argument is a valid host
+            Path torrentFile = Paths.get(args[0]);
+            byte[] tfArray;
+            try {
+                //Read torrent file into a byte array
+                tfArray = Files.readAllBytes(torrentFile);
+                //ToolKit.printString(tfArray, false, 1);
+                try {
+                    //ByteBuffer torrentBuffer = Bencoder2.getInfoBytes(tfArray);
+                    //ToolKit.printString(torrentBuffer, false, 10);
+                    //Decode the torrent byte array
+                    Object torrentDecode = Bencoder2.decode(tfArray);
+                    ToolKit.print(torrentDecode, 0);
+                } catch (BencodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+            // Check if second argument is a valid port number
+            Path saveFile = Paths.get(args[0]);
+            try {
+            HttpRequest.sendGet();
+		    } catch (Exception e){
+		    	e.printStackTrace();
+		    }        
+            
+        //System.out.print("Torrent_File: " + args[0] + "\n" + "File_Save_Name: " + args[1] + "\n");
+        
+        }
+        //End argument check
+    }
+}
